0e34c20b52424304cacec1181ab2b59bc056ca06
ArtsemKozel <artsem86@gmail.com>
2019-02-19 17:52:59 +0100

    js file

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Задание 1
+var Tc;
+var Tf;
+Tc = prompt('Температура по Цельсию');
+Tf = (9 / 5) * Tc + 32;
+alert(Tf);
+*/
+
+/* Задание 3
+var name = prompt('Введите имя');
+var admin = name;
+alert(admin);
+*/
+
+/* Задание 4
+var name=1000+'108';
+alert(name); // 1000108
+*/
